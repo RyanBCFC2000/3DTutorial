42168b527d58b0d5cdbf91e12e794e25eca45c2d
RyanBCFC2000 <45456015+RyanBCFC2000@users.noreply.github.com>
2019-10-22 15:17:55 +0100

    1st
    lol

---
+++ Assets/Scripts/RTSStyle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RTSStyle : MonoBehaviour
+{
+    public Transform target;
+    UnityEngine.AI.NavMeshAgent agent;
+   
+    void Start()
+    {
+       agent = GetComponent<NavMeshAgent>(); 
+    }
+
+
+    void Update()
+    {
+      if (Input.GetMouseButton (0)){
+          //holds the info about raycast
+          RaycastHit hit;
+
+          //launch a raycast from the middle of the camera to a point
+          if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)){
+              agent.destination = hit.point;
+          }
+      }  
+    }
+}
---
+++ Assets/Scripts/WayPoints.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WayPoints : MonoBehaviour
+{
+    UnityEngine.AI.NavMeshAgent agent;
+    //List or Array of points which starts at number 0
+    public GameObject waypoints;
+    Transform[] points;
+    public int destPoint = 0;
+ 
+    void Start()
+    {
+        points = waypoints.GetComponentsInChildren<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+        //Can turn off autobraking to not stop between waypoints
+        agent.autoBraking = false;
+        GotoNextPoint ();
+    }
+    
+    void GotoNextPoint()
+    {
+        if (points.Length == 0){
+            return;
+        }
+
+        agent.SetDestination (points [destPoint].position);
+        //if goes higher than the total number of waypoints -> go back to start of array
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance < 0.5f){
+            GotoNextPoint ();
+        }
+    }
+}
