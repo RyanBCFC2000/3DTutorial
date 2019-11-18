e18d7832c7d674364a4c5979da71392bac41df0c
RyanBCFC2000 <45456015+RyanBCFC2000@users.noreply.github.com>
2019-11-18 15:00:24 +0000

    5th

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -36,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
         {
             if (Input.GetButtonDown("Jump"))
             {
+                anim.SetTrigger("Jump");
                 yVelocity = jumpSpeed;
             }
 
