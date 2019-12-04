34a5c0a7007c86222b844582edc623c041aa66a4
RyanBCFC2000 <45456015+RyanBCFC2000@users.noreply.github.com>
2019-12-04 16:25:20 +0000

    13th

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Stat shield;
+
+    private void Awake()
+    {
+        health.Initialize();
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            shield.CurrentVal -= 10;
+        }
+           if (Input.GetKeyDown(KeyCode.E))
+        {
+            shield.CurrentVal += 10;
+        }
+    }
+}
---
+++ Assets/Scripts/ShieldBarScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldBarScript : MonoBehaviour
+{
+    [SerializeField]
+    private float fillAmount;
+
+    [SerializeField]
+    private Image content;
+
+    public float MaxValue { get; set; }
+
+    public float Value
+    {
+        set
+        {
+           fillAmount = Map(value, 0, MaxValue, 0, 1); 
+        }
+    }
+
+    void Start()
+    {
+      Value = 10;  
+    }
+
+    void Update()
+    {
+      HandleBar();
+    }
+    private void HandleBar()
+    {
+        if (fillAmount != content.fillAmount)
+        {
+            content.fillAmount = fillAmount;
+        }
+   
+
+    }
+
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+        //(78 - 0) * (1 - 0) / (230 - 0) + 0 
+        // 78 * 1 / 230 = 0,339
+    }
+}
