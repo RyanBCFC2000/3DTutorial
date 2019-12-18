bd8ed9a3003f123fffe64f3e94f2a99fbf599176
RyanBCFC2000 <45456015+RyanBCFC2000@users.noreply.github.com>
2019-12-18 16:12:54 +0000

    18th

--- Assets/Scripts/shieldPickup.cs
+++ Assets/Scripts/shieldPickup.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class shieldPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  void OnTriggerEnter(Collider collider){
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        //print("pick up");
+        ShieldScript shield = collider.GetComponent<ShieldScript>();
+        if (shield != null&&collider.tag == "Player"){
+            GetComponent<MeshRenderer>().enabled = false;
+            shield.Damage(-10); //Works out whether the Players shield has dropped below 10, if it has then regain 10 health for the pickup
+            Destroy(gameObject); //Once the Player has picked up the Shield have it Destroy itself
+        }
+  }
 }
