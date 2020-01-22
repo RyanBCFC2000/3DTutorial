e3005abc02085aa1671ea537e8bf25a334cb5e79
RyanBCFC2000 <45456015+RyanBCFC2000@users.noreply.github.com>
2020-01-22 16:22:08 +0000

    23rd

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy 
+{
+    string name;
+    float health;
+    int damageDealt;
+
+    public Enemy(){
+        this.name = "";
+        this.health = 100;
+    }
+
+    public Enemy(string Snake){
+        this.name = Snake;
+        this.health = 100;
+    }
+
+    public Enemy(string Spider){
+        this.name = Spider;
+        this.health = 100;
+    }
+ 
+}
